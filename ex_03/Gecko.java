81fd22e517a4da356345026e29a119b7c8d04b30
Bryan <bryan.ferrando@epitech.eu>
2023-10-24 08:41:19 +0100

    Ex03

---
+++ ex_03/Gecko.java
@@ -0,0 +1,18 @@
+public class Gecko {
+
+    private String name;
+
+    public Gecko(String name) {
+        this.name = name;
+        System.out.println("Hello " + name + "!");
+    }
+
+    public Gecko() {
+        this.name = "Unknown";
+        System.out.println("Hello " + name + "!");
+    }
+
+    public String getName() {
+        return name;
+    }
+}
